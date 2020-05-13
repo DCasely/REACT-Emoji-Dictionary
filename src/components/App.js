9f71b18850b7f388d07165f2b01f880ebcf12559
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-12 22:11:58 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import EmojiCard from './EmojiCard';
+import emojis from '../emojis';
+
+export default function App() {
+  return (
+    <div>
+      <h1>
+        <span>React: Props & Mapping Components for Emoji Dictionary</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojis.map((emoji) => (
+          <EmojiCard
+            key={emoji.id}
+            emoji={emoji.emoji}
+            name={emoji.name}
+            description={emoji.meaning}
+          />
+        ))}
+      </dl>
+    </div>
+  );
+}
