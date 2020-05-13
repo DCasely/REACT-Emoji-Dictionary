4831c63aef235d3aa820cb293f62e2778bec26db
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-05-13 09:13:14 -0400

    headline updated

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="container">
       <h1>
-        <span>React: Props & Mapping Components for Emoji Dictionary</span>
+        <span>REACT (Props & Mapping Components): Emoji Dictionary</span>
       </h1>
 
       <dl className="dictionary">
